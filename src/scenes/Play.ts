6e0110ff2f82d1d0d628d2e743fe7662a9d01fe2
aplaxco1 <aplaxco@ucsc.edu>
2023-10-18 10:04:51 -0700

    end of solo development

--- src/scenes/Play.ts
+++ src/scenes/Play.ts
@@ -3,14 +3,26 @@ import * as Phaser from "phaser";
 import starfieldUrl from "/assets/starfield.png";
 
 export default class Play extends Phaser.Scene {
+  gameWidth?: number;
+  gameHeight?: number;
+
   fire?: Phaser.Input.Keyboard.Key;
   left?: Phaser.Input.Keyboard.Key;
   right?: Phaser.Input.Keyboard.Key;
 
   starfield?: Phaser.GameObjects.TileSprite;
-  spinner?: Phaser.GameObjects.Shape;
+  player?: Phaser.GameObjects.Shape;
+  playerSize: number = 25;
 
-  rotationSpeed = Phaser.Math.PI2 / 1000; // radians per millisecond
+  movementSpeed: number = 2;
+  fireSpeed: number = 1;
+  isFiring: boolean = false;
+
+  leftBound?: number;
+  rightBound?: number;
+
+  enemies?: Phaser.GameObjects.Shape[];
+  numEnemies: number = 3;
 
   constructor() {
     super("play");
@@ -27,40 +39,64 @@ export default class Play extends Phaser.Scene {
   }
 
   create() {
+    this.gameWidth = this.game.config.width as number;
+    this.gameHeight = this.game.config.height as number;
+
     this.fire = this.#addKey("F");
     this.left = this.#addKey("LEFT");
     this.right = this.#addKey("RIGHT");
 
     this.starfield = this.add
-      .tileSprite(
-        0,
-        0,
-        this.game.config.width as number,
-        this.game.config.height as number,
-        "starfield",
-      )
+      .tileSprite(0, 0, this.gameWidth, this.gameHeight, "starfield")
       .setOrigin(0, 0);
 
-    this.spinner = this.add.rectangle(100, 100, 50, 50, 0xff0000);
+    this.leftBound = this.playerSize - 5;
+    this.rightBound = this.gameWidth - this.leftBound;
+
+    this.player = this.add.rectangle(
+      this.gameWidth / 2,
+      this.gameHeight - this.playerSize + 5,
+      this.playerSize,
+      this.playerSize,
+      0x3ab883,
+    );
+
+    let enemyHeight = 30;
+    for (let e = 0; e < this.numEnemies; e++) {
+      const enemy = this.add.rectangle(
+        this.gameWidth / 2,
+        enemyHeight,
+        50,
+        25,
+        0xb3350e,
+      );
+      this.enemies?.push(enemy);
+      enemyHeight += 30;
+    }
   }
 
-  update(_timeMs: number, delta: number) {
+  update() {
     this.starfield!.tilePositionX -= 4;
 
-    if (this.left!.isDown) {
-      this.spinner!.rotation -= delta * this.rotationSpeed;
-    }
-    if (this.right!.isDown) {
-      this.spinner!.rotation += delta * this.rotationSpeed;
+    if (this.isFiring == false) {
+      if (this.left!.isDown && this.player!.x > this.leftBound!) {
+        this.player!.x -= this.movementSpeed;
+      }
+      if (this.right!.isDown && this.player!.x < this.rightBound!) {
+        this.player!.x += this.movementSpeed;
+      }
     }
 
     if (this.fire!.isDown) {
-      this.tweens.add({
-        targets: this.spinner,
-        scale: { from: 1.5, to: 1 },
-        duration: 300,
-        ease: Phaser.Math.Easing.Sine.Out,
-      });
+      this.isFiring = true;
+    }
+
+    if (this.isFiring == true) {
+      this.player!.y -= this.fireSpeed;
+      if (this.player!.y < -this.playerSize) {
+        this.player!.y = this.gameHeight! - this.playerSize + 5;
+        this.isFiring = false;
+      }
     }
   }
 }
